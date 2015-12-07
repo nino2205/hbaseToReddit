c9ee06c62bb30a389c582c7c5b480e71d4252f2a
Sören Grzanna <soeren.grzanna@stud.fh-hannover.de>
2015-12-07 05:34:27 +0100

    added redditReader and start it

--- src/main/java/de/hsh/inform/dbparadigm/hbase/main/RunHBaseQueries.java
+++ src/main/java/de/hsh/inform/dbparadigm/hbase/main/RunHBaseQueries.java
@@ -1,10 +1,12 @@
 package de.hsh.inform.dbparadigm.hbase.main;
 
+import de.hsh.inform.dbparadigm.hbase.service.RedditReader;
+
 public class RunHBaseQueries {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		RedditReader reader = new RedditReader("/r/de");
+		reader.start();
 	}
 
 }
