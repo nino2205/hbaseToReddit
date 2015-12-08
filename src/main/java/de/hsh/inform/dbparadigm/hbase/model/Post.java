f933fd5b2c3aca88efe55552e39e87406074f283
Sören Grzanna <soeren.grzanna@stud.fh-hannover.de>
2015-12-09 00:48:31 +0100

    added all model classes

---
+++ src/main/java/de/hsh/inform/dbparadigm/hbase/model/Post.java
@@ -0,0 +1,36 @@
+package de.hsh.inform.dbparadigm.hbase.model;
+
+/**
+ * Created by Sören on 08.12.2015.
+ */
+public class Post implements IEdge {
+    @Override
+    public String getIdentifierString() {
+        return null;
+    }
+
+    @Override
+    public void setIdentifierString(String id) {
+
+    }
+
+    @Override
+    public INode getSource() {
+        return null;
+    }
+
+    @Override
+    public void setSource(INode node) {
+
+    }
+
+    @Override
+    public INode getDestination() {
+        return null;
+    }
+
+    @Override
+    public void setDestination(INode node) {
+
+    }
+}
---
+++ src/main/java/de/hsh/inform/dbparadigm/hbase/model/SubReddit.java
@@ -0,0 +1,38 @@
+package de.hsh.inform.dbparadigm.hbase.model;
+
+import java.util.List;
+
+/**
+ * Created by Sören on 08.12.2015.
+ */
+public class SubReddit implements INode {
+    @Override
+    public String getIdentifierString() {
+        return null;
+    }
+
+    @Override
+    public void setIdentifierString(String id) {
+
+    }
+
+    @Override
+    public List<IEdge> getIncomingEdges() {
+        return null;
+    }
+
+    @Override
+    public void setIncomingEdges(List<IEdge> edges) {
+
+    }
+
+    @Override
+    public List<IEdge> getOutgoingEdges() {
+        return null;
+    }
+
+    @Override
+    public void setOutgoingEdges(List<IEdge> edges) {
+
+    }
+}
