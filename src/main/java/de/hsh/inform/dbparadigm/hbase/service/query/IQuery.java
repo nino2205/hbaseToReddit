267ebfa3a82bd1ca56decbdc70c98277374ddf1b
Sören Grzanna <soeren.grzanna@stud.fh-hannover.de>
2015-12-09 00:47:19 +0100

    added query classes and a interface of this

---
+++ src/main/java/de/hsh/inform/dbparadigm/hbase/service/query/IQuery.java
@@ -0,0 +1,9 @@
+package de.hsh.inform.dbparadigm.hbase.service.query;
+
+/**
+ * Created by Sören on 08.12.2015.
+ */
+public interface IQuery {
+    public String getName();
+    public void run();
+}
---
+++ src/main/java/de/hsh/inform/dbparadigm/hbase/service/query/MaxDegree.java
@@ -0,0 +1,16 @@
+package de.hsh.inform.dbparadigm.hbase.service.query;
+
+/**
+ * Created by Sören on 08.12.2015.
+ */
+public class MaxDegree implements IQuery {
+    @Override
+    public String getName() {
+        return "MaxDegree";
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
