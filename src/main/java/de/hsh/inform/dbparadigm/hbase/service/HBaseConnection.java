638c7c98cb08a594268f78243fab95bbee57a5c1
Sören Grzanna <soeren.grzanna@stud.fh-hannover.de>
2015-12-09 00:50:05 +0100

    frist tries to connect to hbase

--- src/main/java/de/hsh/inform/dbparadigm/hbase/service/HBaseConnection.java
+++ src/main/java/de/hsh/inform/dbparadigm/hbase/service/HBaseConnection.java
@@ -1,5 +1,37 @@
 package de.hsh.inform.dbparadigm.hbase.service;
 
-public class HBaseConnection {
+import com.google.protobuf.ServiceException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
 
+import java.io.IOException;
+
+public class HBaseConnection{
+    public HBaseConnection(String hbaseZookeeperQuorum, String hbaseZookeeperPropertyClientPort, String hbaseMaster) throws IOException {
+        Configuration config = HBaseConfiguration.create();
+
+        config.clear();
+        config.set("hbase.zookeeper.quorum", hbaseZookeeperQuorum);
+        config.set("hbase.zookeeper.property.clientPort",hbaseZookeeperPropertyClientPort);
+        config.set("hbase.master", hbaseMaster);
+
+        try {
+            HBaseAdmin.checkHBaseAvailable(config);
+        } catch (ServiceException e) {
+            System.out.println("hbase not available");
+            e.printStackTrace();
+        }
+
+        Connection connection = ConnectionFactory.createConnection(config);
+
+        Table table = connection.getTable(TableName.valueOf("test"));
+
+        Put p = new Put(Bytes.toBytes("myLittleRow"));
+
+        p.add(Bytes.toBytes("myLittleFamily"), Bytes.toBytes("someQualifier"), Bytes.toBytes("Some Value"));
+    }
 }
