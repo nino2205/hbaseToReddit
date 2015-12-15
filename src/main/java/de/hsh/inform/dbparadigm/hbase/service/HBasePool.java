3006d4dbbb7c9a5de54948af3ecded34a58ed682
doggi <ich20bin>
2015-12-14 19:11:09 -0800

    implements the save method for iedge

--- src/main/java/de/hsh/inform/dbparadigm/hbase/service/HBasePool.java
+++ src/main/java/de/hsh/inform/dbparadigm/hbase/service/HBasePool.java
@@ -1,13 +1,31 @@
 package de.hsh.inform.dbparadigm.hbase.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import de.hsh.inform.dbparadigm.hbase.model.IEdge;
 import de.hsh.inform.dbparadigm.hbase.model.INode;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
 
 public class HBasePool {
+	private Connection connection;
 
-	public void save(IEdge edge) {
+	public HBasePool(Connection connection){
+		this.connection = connection;
+	}
+
+	public void save(IEdge edge) throws IOException{
+		Table table = connection.getTable(TableName.valueOf("comment"));
+		Put p = new Put(Bytes.toBytes("myLittleRow"));
+		p.addColumn(Bytes.toBytes("myLittleFamily"), Bytes.toBytes("someQualifier"), Bytes.toBytes("Some Value"));
+		table.put(p);
+	}
+
+	public void save(INode node){
 
 	}
 
@@ -18,9 +36,9 @@ public class HBasePool {
 	public void load(INode node) {
 
 	}
-	
+
 	public void load(List<String> node) {
 
 	}
-	
+
 }
